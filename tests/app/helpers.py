db5e0b06a80df66edf2b033ac9d3f7619e23f822
Alexey Bezhan <al@fueledbylemons.com>
2015-05-18 14:23:17 +0100

    Make setup/teardown_authorization standalone functions
    Keeping them as methods on BaseApplicationTest makes it harder
    to reuse the logic for non-class-based tests.

--- tests/app/helpers.py
+++ tests/app/helpers.py
@@ -23,29 +23,27 @@ class BaseApplicationTest(object):
         self.client = self.app.test_client()
         self.default_index_name = "index-to-create"
 
-        self.setup_authorization()
-
-    def setup_authorization(self):
-        """Set up bearer token and pass on all requests"""
-        valid_token = 'valid-token'
-        self.app.wsgi_app = WSGIApplicationWithEnvironment(
-            self.app.wsgi_app,
-            HTTP_AUTHORIZATION='Bearer {}'.format(valid_token))
-        self._auth_tokens = os.environ.get('DM_SEARCH_API_AUTH_TOKENS')
-        os.environ['DM_SEARCH_API_AUTH_TOKENS'] = valid_token
+        setup_authorization(self.app)
 
     def do_not_provide_access_token(self):
         self.app.wsgi_app = self.app.wsgi_app.app
 
     def teardown(self):
         self.client.delete('/' + self.default_index_name)
-        self.teardown_authorization()
+        teardown_authorization()
 
-    def teardown_authorization(self):
-        if self._auth_tokens is None:
-            del os.environ['DM_SEARCH_API_AUTH_TOKENS']
-        else:
-            os.environ['DM_SEARCH_API_AUTH_TOKENS'] = self._auth_tokens
+
+def setup_authorization(app):
+    """Set up bearer token and pass on all requests"""
+    valid_token = 'valid-token'
+    app.wsgi_app = WSGIApplicationWithEnvironment(
+        app.wsgi_app,
+        HTTP_AUTHORIZATION='Bearer {}'.format(valid_token))
+    os.environ['DM_SEARCH_API_AUTH_TOKENS'] = valid_token
+
+
+def teardown_authorization():
+    del os.environ['DM_SEARCH_API_AUTH_TOKENS']
 
 
 def default_service(**kwargs):
