8dfea33473ff08c2df626c38150f427d99891eca
Alexey Bezhan <al@fueledbylemons.com>
2016-05-12 14:30:11 +0100

    Don't set environment variables in run_* scripts

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -5,10 +5,6 @@ else
   source ./venv/bin/activate 2>/dev/null && echo "Virtual environment activated."
 fi
 
-# Use default environment vars for localhost if not already set
-export DM_SEARCH_API_AUTH_TOKENS=${DM_SEARCH_API_AUTH_TOKENS:=myToken}
-export DM_ELASTICSEARCH_URL=${DM_ELASTICSEARCH_URL:=http://localhost:9200}
-
 echo "Environment variables in use:"
 env | grep DM_
 
