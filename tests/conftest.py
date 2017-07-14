c0df1342b6625cdc2a205f2ba13ee201e8d0b02a
George Lund <george.lund@digital.cabinet-office.gov.uk>
2017-07-14 11:17:24 +0100

    Use with block to start/stop the patch context manager.
     - this is less code, hopefully is just as clear why we need to 'yield'
       rather than just 'return'.
    
    https://trello.com/c/OpWI068M/380-after-g9-go-live-removal-of-old-filters-from-search-api-mapping

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,10 +16,6 @@ with open(os.path.join(os.path.dirname(__file__), 'fixtures/mappings/services.js
 def services_mapping():
     """Provide a services mapping fixture, and patch it into the global singleton getter."""
 
-    mock_services_mapping_getter_patch = mock.patch('app.mapping.get_services_mapping')
-    mock_services_mapping_getter = mock_services_mapping_getter_patch.start()
-    mock_services_mapping_getter.return_value = app.mapping.Mapping(_services_mapping_definition, 'services')
-
-    yield mock_services_mapping_getter.return_value
-
-    mock_services_mapping_getter_patch.stop()
+    with mock.patch('app.mapping.get_services_mapping') as mock_services_mapping_getter:
+        mock_services_mapping_getter.return_value = app.mapping.Mapping(_services_mapping_definition, 'services')
+        yield mock_services_mapping_getter.return_value
