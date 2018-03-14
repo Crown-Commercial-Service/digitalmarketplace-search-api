e6f615daa700fe052c67ad7c11ea145090ec18bb
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-03-14 15:21:08 +0000

    Use centralised status method
     ## Summary
    The status endpoint for all of the apps is highly redundant across
    repositories so I've moved it to dm-utils. We now only need to call this
    method from the view stub in each app and pass in a couple of resources
    (mainly the current_app and any api clients) to get a consistent status
    JSON blob back.
    
     ## Ticket
    https://trello.com/c/BBvu6eUk/383-ensure-healthcheck-endpoint-fails-if-low-disk-space

--- app/status/views.py
+++ app/status/views.py
@@ -1,38 +1,24 @@
-from flask import jsonify, current_app, request
+from flask import request
 
 from . import status
 from ..main.services.search_service import status_for_all_indexes
-from dmutils.status import get_flags
+from dmutils.status import get_app_status, StatusError
+
+
+def get_es_status():
+    result, status_code = status_for_all_indexes()
+
+    if status_code != 200:
+        raise StatusError(f'Error connecting to elasticsearch (status_code: {status_code}, message: {result})')
+
+    return {
+        'es_status': result
+    }
 
 
 @status.route('/_status')
 def status():
-
-    if 'ignore-dependencies' in request.args:
-        return jsonify(
-            status="ok",
-        ), 200
-
-    result, status_code = status_for_all_indexes()
-    version = current_app.config['VERSION']
-
-    if status_code == 200:
-        return jsonify(
-            status="ok",
-            version=version,
-            es_status=result,
-            flags=get_flags(current_app)
-        )
-
-    current_app.logger.exception("Error connecting to elasticsearch")
-
-    return jsonify(
-        status="error",
-        version=version,
-        message="Error connecting to elasticsearch",
-        es_status={
-            'status_code': status_code,
-            'message': "{}".format(result),
-        },
-        flags=get_flags(current_app)
-    ), 500
+    return get_app_status(data_api_client=None,
+                          search_api_client=None,
+                          ignore_dependencies='ignore-dependencies' in request.args,
+                          additional_checks=[get_es_status])
